9bfbf679ee0b6bd9ec33da0505351d84d13d688a
Aliciac07 <20170465@ce.pucmm.edu.do>
2021-09-29 14:38:48 -0400

    Realizacion de los codigos de http status.

--- practica-2-PWA/src/main/java/practica/mocky/practica2pwa/repositories/UserRepository.java
+++ practica-2-PWA/src/main/java/practica/mocky/practica2pwa/repositories/UserRepository.java
@@ -1,10 +1,11 @@
 package practica.mocky.practica2pwa.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import practica.mocky.practica2pwa.models.User;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findUserByUsernameAndActiveTrue(String name);
 
