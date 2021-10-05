4eda50b83363bde90a769b4b46757dd9f481005d
Rubencrash236 <45013989+Rubencrash236@users.noreply.github.com>
2021-10-05 01:48:44 -0400

    Create form working

--- front-practica-2-PWA/src/app/app-routing.module.ts
+++ front-practica-2-PWA/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateMockupsComponent } from './components/create-mockups/create-mockups.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'landing', component: LandingComponent},
+  {path:'dashboard', component: DashboardComponent},
+  {path:'create', component: CreateMockupsComponent},
   {path: '', pathMatch:'full', redirectTo: 'landing'},
 
 ];
