582bc1ffd46c54ff734c57cf89a4f353b10ec9b1
Rubencrash236 <45013989+Rubencrash236@users.noreply.github.com>
2021-11-01 12:55:13 -0400

    Fix WS controller and message sender system

--- practica-4-PWA/src/main/java/jms/practica4pwa/controller/MessageController.java
+++ practica-4-PWA/src/main/java/jms/practica4pwa/controller/MessageController.java
@@ -2,31 +2,40 @@ package jms.practica4pwa.controller;
 
 import jms.practica4pwa.models.Message;
 import jms.practica4pwa.services.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableScheduling
 @Controller
 public class MessageController {
 
     private final MessageService messageService;
-
+    @Autowired
+    private SimpMessagingTemplate template;
     public MessageController(MessageService messageService) {
         this.messageService = messageService;
     }
 
-    @MessageMapping("/all-data")
-    @SendTo("/topic/data")
-    public Iterable<Message> findAll(){
-        return messageService.findAll();
+    //@SendTo("/topic/data")
+    //@Scheduled(fixedRate = 10000)
+    public void findAll(){
+        this.template.convertAndSend("/topic/data", messageService.findAll());
     }
+    /*public Iterable<Message> findAll(){
+        return messageService.findAll();
+    }*/
 
-    @MessageMapping("/data-co")
+    /*@MessageMapping("/data-co")
     @SendTo("/topic/data")
     public Message findById(Integer id){
         return messageService.findById(id);
-    }
+    }*/
 }
--- practica-4-front-PWA/src/app/graphs/graphs.component.ts
+++ practica-4-front-PWA/src/app/graphs/graphs.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import Message from '../models/message';
 import { Client } from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import { prepareEventListenerParameters } from '@angular/compiler/src/render3/view/template';
 
 @Component({
   selector: 'app-graphs',
@@ -11,6 +12,7 @@ import * as SockJS from 'sockjs-client';
 })
 export class GraphsComponent implements OnInit {
   private client!: Client;
+  connected: boolean = false;
   messages: Message[] = []
   data: any = [];
 
@@ -19,8 +21,30 @@ export class GraphsComponent implements OnInit {
   ngOnInit(): void {
     this.client = new Client();
     this.client.webSocketFactory = function (){
-      return new SockJS("http://localhost:8080/");
+      return new SockJS("http://localhost:8080/jms-websocket");
     }
+    this.client.onConnect = (frame) =>{
+      console.log('Conected: '+ this.client.connected + ' : ' + frame);
+      this.connected = true;
+      console.log('trying to subscribe');
+      this.client.subscribe('/topic/data', e =>{
+        console.log(e.body);
+      });
+      //this.client.publish({destination:'/app/all-data'});
+    }
+
+    this.client.onDisconnect = (frame) =>{
+      console.log('Disconnected: ' + !this.client.connected + ' : '+frame);
+      this.connected = false;
+    }
+  }
+
+  connect(): void {
+    this.client.activate();
+  }
+
+  disconnect(): void {
+    this.client.deactivate();
   }
 
 }
