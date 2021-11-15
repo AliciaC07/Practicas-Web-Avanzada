777960949f688c27a92ba3d3790188e18a33161a
KillerCode_236 <45013989+Rubencrash236@users.noreply.github.com>
2021-11-14 23:54:59 -0400

    Update main

--- practica-5-PWA/src/main/java/practica/mocky/practica2pwa/Practica3PwaApplication.java
+++ practica-5-PWA/src/main/java/practica/mocky/practica2pwa/Practica3PwaApplication.java
@@ -9,5 +9,10 @@ public class Practica3PwaApplication {
     public static void main(String[] args) {
         SpringApplication.run(Practica3PwaApplication.class, args);
     }
+    
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "8082");
+    }
 
 }
