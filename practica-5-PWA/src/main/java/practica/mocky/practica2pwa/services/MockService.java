126bd2c55f72614dde3130ec8d9c812a36a6f4d6
root <root@ip-172-31-16-146.ec2.internal>
2021-11-16 01:56:06 +0000

    Finished build

--- practica-5-PWA/src/main/java/practica/mocky/practica2pwa/services/MockService.java
+++ practica-5-PWA/src/main/java/practica/mocky/practica2pwa/services/MockService.java
@@ -28,7 +28,7 @@ public class MockService {
     private final UserService userService;
     private final JwGen jwGen = new JwGen();
     private final RamEncoder ramEncoder = new RamEncoder();
-    private String endpoint = "http://localhost:8080/";
+    private String endpoint = "https://mockys.traki-tech.games/";
 
     public MockService(MockRepository mockRepository, HttpStatusCodeService httpStatusCodeService, UserService userService) {
         this.mockRepository = mockRepository;
